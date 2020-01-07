becd77093788055e301c340a3be2972bba90aa0b
hettipower <hettipower@gmail.com>
2020-01-07 07:56:38 +0530

    whyMarketing Component style

--- src/components/why-marketing/why-marketing.component.jsx
+++ src/components/why-marketing/why-marketing.component.jsx
@@ -4,6 +4,8 @@ import './why-marketing.styles.scss';
 
 const WhyMarketing = () => (
     <div className="whyMarketingWrap text-center">
+        <h6>Why do you need marketing ?</h6>
+        <h2>Having a great business without marketing is like owning a super car <span>without an engine</span></h2>
         <span className="btn blueBtn">Get me an Engine</span>
     </div>
 );

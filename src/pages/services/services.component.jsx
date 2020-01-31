2d1d4394efed99b7cf397ca68f0146d68b9d32a4
hettipower <hettipower@gmail.com>
2020-01-31 12:07:34 +0530

    update service page

--- src/pages/services/services.component.jsx
+++ src/pages/services/services.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import MainBanner from '../../components/services/main-banner/main-banner.component';
+
 import './services.styles.scss';
 
 const ServicesPage = () => (
     <div className="servicePageWrap">
-        
+        <MainBanner />
     </div>
 );
 
